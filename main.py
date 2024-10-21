e4a30da6423427c8e014ad987827373d1fb84ad7
stakos86 <147206155+stakos86@users.noreply.github.com>
2024-10-21 19:51:03 +0300

    работа с архивами

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import os
+import zipfile
 
 print(os.path.abspath(__file__))
+
+with zipfile.ZipFile('/Users/Stanislav_Egorov/Documents/GitHub/blitz-croco-words/src/croco-blitz-source.zip', 'r') as zip_file:
+    # Получаем список файлов
+    file_list = zip_file.namelist()
+
+    print("Список файлов в архиве:")
+    for file in file_list:
+        print(file)
