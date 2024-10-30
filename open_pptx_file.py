ca8568d8a0e98519ac7198a98c11232809b85813
stakos86 <147206155+stakos86@users.noreply.github.com>
2024-10-30 20:13:45 +0300

    2024.10.30.01

--- open_pptx_file.py
+++ open_pptx_file.py
@@ -1,23 +1,118 @@
+# from pptx import Presentation
+#
+# # Укажите путь к файлу презентации
+# file_path = '/Users/Stanislav_Egorov/Documents/GitHub/blitz-croco-words/src/croco-blitz-source/Osennyaya_igra_3.pptx'
+#
+# # Открываем файл презентации
+# presentation = Presentation(file_path)
+#
+# # Теперь вы можете работать с объектом presentation
+# # Например, выводим количество слайдов
+# # print(f'Количество слайдов в презентации: {len(presentation.slides)}')
+#
+# # Вы можете итерироваться по слайдам
+# # for slide in presentation.slides:
+# #     print(f'Слайд {slide.slide_id}')
+#
+# prs = Presentation('Osennyaya_igra_3.pptx')
+
+# for slide in prs.slides:
+#     for shape in slide.shapes:
+#         if not shape.has_text_frame:
+#             continue
+#         print(shape.text)
+
+
+# def extract_words_without_spaces(file_path):
+#     prs = Presentation(file_path)
+#
+#     words_without_spaces = []
+#     for slide in prs.slides:
+#         for shape in slide.shapes:
+#             if hasattr(shape, 'text'):
+#                 text = shape.text.strip()
+#                 words = text.split()
+#                 if len(words) == 1:
+#                     words_without_spaces.append(words[0])
+#
+#     return words_without_spaces
+#
+#
+# extracted_words_without_spaces = extract_words_without_spaces(file_path)
+# for word in extracted_words_without_spaces:
+#     print(word)
+
+from zipfile import ZipFile
+from pathlib import Path
 from pptx import Presentation
+from random import choice
 
-# Укажите путь к файлу презентации
-file_path = '/Users/Stanislav_Egorov/Documents/GitHub/blitz-croco-words/src/croco-blitz-source/Osennyaya_igra_3.pptx'
+# Константы
+SRC = 'src'
 
-# Открываем файл презентации
-presentation = Presentation(file_path)
+# Словарь с номерами и соответствующими им именами файлов презентаций
+PPTX_FILES_LIST: list[str] = [
+    "Osennyaya_igra_3.pptx",
+    "Zimnyaya_igra_1.pptx",
+    "Osennyaya_igra_12.pptx",
+    "Osennyaya_igra_11.pptx",
+    "Osennyaya_igra_10.pptx",
+    "Osennyaya_igra_9.pptx",
+    "Osennyaya_igra_6.pptx",
+    "Osennyaya_igra_5.pptx",
+    "Osennyaya_igra_4.pptx"
+]
 
-# Теперь вы можете работать с объектом presentation
-# Например, выводим количество слайдов
-print(f'Количество слайдов в презентации: {len(presentation.slides)}')
 
-# Вы можете итерироваться по слайдам
-for slide in presentation.slides:
-    print(f'Слайд {slide.slide_id}')
+def open_pptx_with_pptx_library(presentation_path):
+    prs = Presentation(presentation_path)
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, 'text'):
+                print(shape.text)
 
-prs = Presentation('Osennyaya_igra_3.pptx')
 
-for slide in prs.slides:
-    for shape in slide.shapes:
-        if not shape.has_text_frame:
-            continue
-        print(shape.text)
+def extract_words_from_pptx(presentation_path):
+    words = []
+    prs = Presentation(presentation_path)
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if hasattr(shape, 'text'):
+                text = shape.text
+                if ' ' in text or ':' in text or 'БЛИЦ-КРОКОДИЛ' in text:
+                    continue
+                words.append(text.strip())
+    return words
+
+
+def write_txt(filename: str, words: list[str]):
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            for word in words:
+                f.write(word + '\n')
+        print(f"Слова успешно записаны в файл {filename}")
+    except IOError as e:
+        print(f"Ошибка при записи в файл: {e}")
+
+
+def main() -> None:
+    # Извлекаем файл презентации
+    # extract_pptx_from_zip(SRC, FILENAME_ZIP, EXTRACTION_DIR)
+
+    # Открываем и выводим содержимое выбранной презентации
+    presentation_path = '/Users/Stanislav_Egorov/Documents/GitHub/blitz-croco-words/src/croco-blitz-source'
+    # open_pptx_with_pptx_library(str(presentation_path))
+
+    # Выбираем случайный файл презентации
+    pptx_file_name = 'Osennyaya_igra_11.pptx'
+    presentation_path = Path(presentation_path) / pptx_file_name
+    # open_pptx_with_pptx_library(str(presentation_path))
+
+    # Извлекаем слова из презентации
+    extracted_words = extract_words_from_pptx(str(presentation_path))
+
+    # Записываем извлеченные слова в txt-файл
+    write_txt('words.txt', extracted_words)
+
+
+main()
