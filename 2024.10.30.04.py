8530cd27851afb9772776ffe4bef0c15dd01aa90
stakos86 <147206155+stakos86@users.noreply.github.com>
2024-11-06 20:55:15 +0300

    С проверкой YandexSpeller

--- 2024.10.30.04.py
+++ 2024.10.30.04.py
@@ -1,5 +1,6 @@
 from View_files_in_zip import *
 from open_pptx_file import *
+from pyaspeller import YandexSpeller
 
 def main() -> None:
     extract_pptx_from_zip('/Users/Stanislav_Egorov/Documents/GitHub/blitz-croco-words/src/croco-blitz-source.zip'
@@ -9,4 +10,7 @@ def main() -> None:
 
     extracted_words = extract_words_from_pptx(str(presentation_path))
 
-    write_txt('words.txt', extracted_words)
+    speller = YandexSpeller()
+    result = speller.spelled('extracted_words')
+
+    write_txt('words.txt', result)
--- check_spell.py
+++ check_spell.py
@@ -1,6 +1,7 @@
 from pyaspeller import YandexSpeller
 
 if __name__ == "__main__":
+    spisok = list["Колакал", "малако"]
     speller = YandexSpeller()
-    result = speller.spelled('Колакал малако')
+    result = speller.spelled(spisok)
     print(result)
