f38ec6e44a47f1db6b1c382c0955643865b23312
stakos86 <147206155+stakos86@users.noreply.github.com>
2024-11-15 19:18:37 +0300

    table movie already exists

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ import sqlite3
 def main() -> None:
     con = sqlite3.connect('words.db')
     cur = con.cursor()
+    cur.execute('CREATE TABLE movie(title, year, score)')
 
 
 if __name__ == '__main__':
