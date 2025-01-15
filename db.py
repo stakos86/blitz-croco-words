bae1a0f3570f8b3ee47ad6c190dd51cd74424262
stakos86 <147206155+stakos86@users.noreply.github.com>
2025-01-15 10:18:42 +0300

    Некоторые изменения

--- db.py
+++ db.py
@@ -1,11 +1,37 @@
 import sqlite3
 
 
-def main() -> None:
+# def main() -> None:
+#     con = sqlite3.connect('words.db')
+#     cur = con.cursor()
+#     cur.execute('CREATE TABLE IF NOT EXISTS movie(title, year, score)')
+#     res = con.execute('SELECT name FROM sqlite_master')
+#     res = res.fetchone()
+#     print(res)
+
+# def main():
+#     con = sqlite3.connect('words.db')
+#     cur = con.cursor()
+#     cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+#     cur.execute("""
+#         INSERT INTO movie VALUES
+#             ('Monty Python and the Holy Grail', 1975, 8.2),
+#             ('And Now for Something Completely Different', 1971, 7.5)
+#     """)
+#     con.commit()
+
+def main():
     con = sqlite3.connect('words.db')
     cur = con.cursor()
-    cur.execute('CREATE TABLE movie(title, year, score)')
+    cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
 
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0),
+    ]
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    con.commit()
 
 if __name__ == '__main__':
     main()
